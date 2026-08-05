fbec79b74763f76ed5a0b5f2b5904f5298cf74f0
ManideepBogireddy <manideepbogireddy@gmail.com>
2026-08-05 23:36:46 +0530

    Added is the number is perfect number or not using for loop

---
+++ corejava/src/com/corejava/loops/PerfectNumber.java
@@ -0,0 +1,35 @@
+package com.corejava.loops;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+	
+	static boolean perfect(int n) {
+		int sum = 0;
+		for(int i = 1; i <= n/2; i++) {
+			if(n % i == 0) {
+				sum += i;
+			}
+		}
+		return sum == n;
+	
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number : ");
+		int n = sc.nextInt();
+		
+		boolean result = perfect(n);;
+		
+		if(result) {
+			System.out.println(""+n+" is Perfect number.");
+		} else {
+			System.out.println(""+n+" is not a perfect number");
+		}
+		
+		sc.close();
+
+	}
+
+}
