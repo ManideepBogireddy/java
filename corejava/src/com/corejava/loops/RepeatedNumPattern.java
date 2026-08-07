f5566f90d2af5c481c2c383fae3380f3c8803426
ManideepBogireddy <manideepbogireddy@gmail.com>
2026-08-07 23:53:18 +0530

    Added Repeated Number Pattern Problem

---
+++ corejava/src/com/corejava/loops/RepeatedNumPattern.java
@@ -0,0 +1,30 @@
+package com.corejava.loops;
+
+import java.util.Scanner;
+
+public class RepeatedNumPattern {
+	
+	static void repeatedNum(int n) {
+		for(int i = 1; i<=n; i++) {
+			for(int j = 1; j<=i; j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println("");
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		
+		if(sc.hasNextInt()) {
+			int n = sc.nextInt();
+			
+			repeatedNum(n);
+		}
+		sc.close();
+
+	}
+
+}
