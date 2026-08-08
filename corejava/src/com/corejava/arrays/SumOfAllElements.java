ca74ae83ef1014a47cb3d33fb9829b9e49d66901
ManideepBogireddy <manideepbogireddy@gmail.com>
2026-08-08 23:21:56 +0530

    "Add program to calculate sum of array elements

---
+++ corejava/src/com/corejava/arrays/SumOfAllElements.java
@@ -0,0 +1,29 @@
+package com.corejava.arrays;
+
+import java.util.Scanner;
+
+public class SumOfAllElements {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of the array: ");
+		int size = sc.nextInt();
+
+		int[] arr = new int[size];
+
+		System.out.println("Enter " + size + " elements: ");
+		for (int i = 0; i < size; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int sum = 0;
+
+		for (int j = 0; j < size; j++) {
+			sum = sum + arr[j];
+		}
+		System.out.println("The sum of the elements in array is: " + sum);
+
+		sc.close();
+
+	}
+
+}
