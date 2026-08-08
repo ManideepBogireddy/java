49e8d9870a020fbd5943c4bb8dfb21bae7cd2665
ManideepBogireddy <manideepbogireddy@gmail.com>
2026-08-08 23:20:39 +0530

    Add program to print all array elements

---
+++ corejava/src/com/corejava/arrays/PrintAllElements.java
@@ -0,0 +1,31 @@
+package com.corejava.arrays;
+
+import java.util.*;
+
+public class PrintAllElements {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the size of the array: ");
+		int size = sc.nextInt();
+		
+		int[] arr = new int[size];
+		
+		System.out.print("Enter "+size+" elements: ");
+		for(int i = 0; i<size; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println("Output: ");
+		for(int i = 0; i<size; i++) {
+			System.out.print(arr[i]+ " ");
+		}
+//		System.out.println(Arrays.toString(arr));
+		
+		
+		sc.close();
+
+	}
+
+}
