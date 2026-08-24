935cec553b77054c5e5a961dbbcbb188305ad741
ManideepBogireddy <manideepbogireddy@gmail.com>
2026-08-24 19:05:35 +0530

    add array sum program

---
+++ tasks/src/com/arrays/SumOfElements.java
@@ -0,0 +1,31 @@
+package com.arrays;
+
+import java.util.Scanner;
+
+public class SumOfElements {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the size of the array: ");
+		int size = sc.nextInt();
+
+		int[] arr = new int[size];
+
+		for (int i = 0; i < size; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int sum = 0;
+
+		for (int i = 0; i < size; i++) {
+			sum += arr[i];
+		}
+		
+		System.out.println(sum);
+
+		sc.close();
+
+	}
+
+}
