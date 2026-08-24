60e37486ee1c9fb1899c28a609e326db855b59b8
ManideepBogireddy <manideepbogireddy@gmail.com>
2026-08-24 18:59:53 +0530

    add array occurrence count program

---
+++ tasks/src/com/arrays/CountOccurrence.java
@@ -0,0 +1,43 @@
+package com.arrays;
+
+import java.util.Scanner;
+
+public class CountOccurrence {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the size of the array: ");
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		
+		System.out.println("Enter the elements in the array: ");
+		for(int i = 0; i<size;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		System.out.println("Enter element to search: ");
+		int search = sc.nextInt();
+		
+		
+		int count = 0;
+		
+		for(int i = 0; i<size; i++) {
+			if(arr[i] == search) {
+				count++;
+			}
+		}
+		
+		
+		
+		System.out.println(count);
+		
+		
+		
+		sc.close();
+		
+
+	}
+
+}
