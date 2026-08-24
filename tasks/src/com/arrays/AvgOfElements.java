44a6c739132fe8d22e2fced2368e4f7c31d8b0af
ManideepBogireddy <manideepbogireddy@gmail.com>
2026-08-24 18:59:14 +0530

    add array average program

---
+++ tasks/src/com/arrays/AvgOfElements.java
@@ -0,0 +1,32 @@
+package com.arrays;
+
+import java.util.Scanner;
+
+public class AvgOfElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the size of the array: ");
+		int size = sc.nextInt();
+
+		int[] arr = new int[size];
+
+		for (int i = 0; i < size; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int sum = 0;
+
+		for (int i = 0; i < size; i++) {
+			sum += arr[i];
+		}
+		int avg = sum / size;
+		System.out.println(avg);
+
+		sc.close();
+
+	}
+
+}
